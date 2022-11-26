b2eb197a193cb2a092c392ad6c7e318cd60dfd26
kirillwolkow <kirillwolkow@protonmail.com>
2022-11-26 09:06:41 +0100

    style signup page

--- project/custom_auth/forms.py
+++ project/custom_auth/forms.py
@@ -1,8 +1,21 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
 
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = get_user_model()
-        fields = ("email", "first_name", "last_name")
+        fields = ("email", "first_name", "last_name", "password1", "password2", "is_artist")
+        widgets = {
+            'email': forms.EmailInput(attrs={'class': 'w-full rounded-sm', 'placeholder': 'E-Mail'}),
+            'first_name': forms.TextInput(attrs={'class': 'w-full rounded-sm', 'placeholder': 'First Name'}),
+            'last_name': forms.TextInput(attrs={'class': 'w-full rounded-sm', 'placeholder': 'Last Name'}),
+            'is_artist': forms.CheckboxInput(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+        self.fields['password1'].widget = forms.PasswordInput(attrs={'class': 'form-control w-full rounded-sm', 'placeholder': 'Password'})
+        self.fields['password2'].widget = forms.PasswordInput(attrs={'class': 'form-control w-full rounded-sm', 'placeholder': 'Confirm Password'})
+        self.fields['is_artist'].label = "I'm an artist"
