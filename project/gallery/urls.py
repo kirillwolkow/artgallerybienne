3def2bb98f720cc0e0747162b9e363c5702b330c
kirillwolkow <kirillwolkow@protonmail.com>
2022-12-05 18:13:00 +0100

    create view, path and template for about us page

--- project/gallery/urls.py
+++ project/gallery/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about-us/', views.about_us_view, name='about-us'),
     path('art-create/', views.ArtCreate.as_view(), name='art-create'),
     path('art-list/', views.ArtListView.as_view(), name='art-list'),
     path('art/<int:pk>', views.ArtDetailView.as_view(), name='art'),
